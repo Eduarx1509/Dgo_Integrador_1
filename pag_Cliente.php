e8a530739eef1ac90a137e07821e265b83a34f02
Eduarx1509 <131576418+Eduarx1509@users.noreply.github.com>
2023-11-25 02:04:08 -0500

    c

--- pag_Cliente.php
+++ pag_Cliente.php
@@ -252,7 +252,7 @@ while ($data = $consulta_info->fetch_assoc()) {
                 <a class="footer-subtitulo" href="">Chatea con nosotros</a><br>
                 <a href="" class="footer-texto">Te atendemos las 24 horas</a><br>
                 <a href="">Escríbenos</a><br>
-                <a href="">Chatea con nosotros siempre</a><br>
+                <a href="">Chatea con nosotros siempre...</a><br>
                 <a href="">Te atendemos las 24 horas</a><br>
             </div>
             <div class="footer-contenido">
